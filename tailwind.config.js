94246b062d0430dafbe4d459817d37b8aef08cd7
Fahmii19 <fhmialfi79@gmail.com>
2023-08-10 16:46:17 +0700

    membuat mobile pada beranda pintoinvest dan mengganti icon

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "360px",
+      md: "768px",
+      lg: "1024px",
+    },
+  },
+  plugins: [],
+};
