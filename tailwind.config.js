be99d71960fcd2a88e46d8597f1081726904ba3f
Fahmii19 <fhmialfi79@gmail.com>
2023-09-17 13:05:22 +0700

    menyesuiakn font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans_serif: ["ui-sans-serif", "system-ui"],
+        sans_ui_serif: ["ui-sans-serif"],
       },
     },
     screens: {
